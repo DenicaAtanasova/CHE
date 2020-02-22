9e3562e4b50b9eafc7ba457d3ebe9f37f5639718
Denica Atanasova <d.d.atanasova@gmail.com>
2020-02-22 21:20:39 +0200

    Added cooperative create view and input model

---
+++ Web/CHE.Web.InputModels/Cooperatives/CooperativeCreateInputModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CHE.Web.InputModels.Cooperatives
+{
+    using CHE.Web.InputModels.Attributes.Validation;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CooperativeCreateInputModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Info { get; set; }
+
+        [Required]
+        [Grade]
+        public string Grade { get; set; }
+    }
+}
