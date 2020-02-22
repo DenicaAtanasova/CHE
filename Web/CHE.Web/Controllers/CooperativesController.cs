c977a2c407404c737d54189e967e1d183b554158
Denica Atanasova <d.d.atanasova@gmail.com>
2020-02-22 21:21:11 +0200

    Added cooperatives controller

---
+++ Web/CHE.Web/Controllers/CooperativesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CHE.Services.Data;
+using CHE.Web.InputModels.Cooperatives;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CHE.Web.Controllers
+{
+    public class CooperativesController : Controller
+    {
+        private readonly ICooperativesService _cooperativesService;
+
+        public CooperativesController(ICooperativesService cooperativesService)
+        {
+            this._cooperativesService = cooperativesService;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CooperativeCreateInputModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            var username = this.User.Identity.Name;
+            await this._cooperativesService.CreateAsync(model.Name, model.Info, model.Grade, username);
+            return this.Redirect("/");
+        }
+    }
+}
