4095596b3f8c509a4618ae03cc9aeda7e1c76f53
Denica Atanasova <d.d.atanasova@gmail.com>
2020-02-22 18:15:14 +0200

    Added grade service

---
+++ Services/CHE.Services.Data/GradesService.cs
@@ -0,0 +1,33 @@
+﻿namespace CHE.Services.Data
+{
+    using CHE.Data;
+
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+    using CHE.Data.Models;
+
+    public class GradesService : IGradesService
+    {
+        private readonly CheDbContext _dbContext;
+
+        public GradesService(CheDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<Grade> GetByValue(string value)
+        {
+            var gradeFromDb = await this._dbContext.Grades
+                .FirstOrDefaultAsync(x => x.Value == value);
+
+            return gradeFromDb;
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync<TEntity>()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Services/CHE.Services.Data/IGradesService.cs
@@ -0,0 +1,13 @@
+﻿namespace CHE.Services.Data
+{
+    using CHE.Data.Models;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IGradesService
+    {
+        Task<Grade> GetByValue(string value);
+
+        Task<IEnumerable<TEntity>> GetAllAsync<TEntity>();
+    }
+}
