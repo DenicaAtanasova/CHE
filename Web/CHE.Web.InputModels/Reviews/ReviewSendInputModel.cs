a01deca497742a49bf35878bc88fe11a5690d0dd
Denica Atanasova <d.d.atanasova@gmail.com>
2020-03-02 17:09:31 +0200

    Added reviews send view

---
+++ Web/CHE.Web.InputModels/Reviews/ReviewSendInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CHE.Web.InputModels.Reviews
+{
+    public class ReviewSendInputModel
+    {
+        public string Comment { get; set; }
+
+        public int Rating { get; set; }
+
+        public string ReceiverId { get; set; }
+    }
+}
