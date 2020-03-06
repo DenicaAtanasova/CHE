0464a862bfcceaccf172d498781dca22a2938fa0
Denica Atanasova <d.d.atanasova@gmail.com>
2020-03-06 11:17:41 +0200

    Added common library project with global constants

---
+++ Common/CHE.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace CHE.Common
+{
+    public static class GlobalConstants
+    {
+        public const string TEACHER_ROLE = "Teacher";
+
+        public const string PARENT_ROLE = "Parent";
+    }
+}
