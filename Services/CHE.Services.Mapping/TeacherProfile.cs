bfadf7bc655e6111489544b1229050f0678bcd93
Denica Atanasova <d.d.atanasova@gmail.com>
2020-03-06 12:02:00 +0200

    Fixed: rating visualization

--- Services/CHE.Services.Mapping/TeacherProfile.cs
+++ Services/CHE.Services.Mapping/TeacherProfile.cs
@@ -6,13 +6,14 @@
     using CHE.Web.ViewModels.Teachers;
 
     using AutoMapper;
+    using System;
 
     public class TeacherProfile : Profile
     {
         public TeacherProfile()
         {
             this.CreateMap<CheUser, TeacherAllViewModel>()
-                .ForMember(dest => dest.Rating, opt => opt.MapFrom(src => src.ReviewsReceived.Average(x => x.Rating)))
+                .ForMember(dest => dest.Rating, opt => opt.MapFrom(src => Math.Round(src.ReviewsReceived.Average(x => x.Rating), 1)))
                 .ForMember(dest => dest.ReviewsCount, opt => opt.MapFrom(src => src.ReviewsReceived.Count));
 
             this.CreateMap<CheUser, TeacherDetailsViewModel>();
