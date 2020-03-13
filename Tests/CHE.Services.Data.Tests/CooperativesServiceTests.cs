a6ee9ac7a0224cfc718a7717e2140245594cfae0
Denica Atanasova <d.d.atanasova@gmail.com>
2020-03-13 11:14:38 +0200

    Added CooperativesServiceTests class with CreateAsync method test

---
+++ Tests/CHE.Services.Data.Tests/CooperativesServiceTests.cs
@@ -0,0 +1,53 @@
+﻿namespace CHE.Services.Data.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class CooperativesServiceTests : BaseTest
+    {
+        private readonly string ID = Guid.NewGuid().ToString();
+        private const string NAME = "Test name";
+        private const string INFO = "Test info";
+        private const string GRADE = "First";
+
+        private readonly CooperativesService _cooperativesService;
+
+        public CooperativesServiceTests()
+            : base()
+        {
+            this._cooperativesService = new CooperativesService(
+                this.DbContext,
+                this.Mapper,
+                new GradesService(this.DbContext));
+        }
+
+        // CreateAsync
+        [Fact]
+        public async Task CreateAsyncShouldCreateCooperative()
+        {
+            var createSuccessful = await this._cooperativesService
+                .CreateAsync(NAME, INFO, GRADE, ID);
+
+            Assert.True(createSuccessful);
+        }
+
+        // UpdateAsync
+
+        // DeleteAsync
+
+        // GetByIdAsync
+
+        // GetAllAsync
+
+        // GetCreatorAllByUsernameAsync
+
+        // GetJoinRequestsAsync
+
+        // AddMemberAsync
+
+        // RemoveMemberAsync
+
+        // LeaveAsync
+    }
+}
