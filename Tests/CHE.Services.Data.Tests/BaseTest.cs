604a22e283a5f5e3e418f1d42387c1c092623c44
Denica Atanasova <d.d.atanasova@gmail.com>
2020-03-13 12:15:51 +0200

    Added serviceProvider to BaseTest. Configured autoMapper.

--- Tests/CHE.Services.Data.Tests/BaseTest.cs
+++ Tests/CHE.Services.Data.Tests/BaseTest.cs
@@ -1,41 +1,57 @@
 ﻿namespace CHE.Services.Data.Tests
 {
     using System;
+
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
 
     using AutoMapper;
 
-    using CHE.Data;
     using CHE.Services.Mapping;
+    using CHE.Data;
 
     public class BaseTest : IDisposable
     {
+        private readonly ServiceProvider _serviceProvider;
         private readonly CheDbContext _dbContext;
+        private readonly IMapper _mapper;
 
         public BaseTest()
         {
-            this._dbContext = this.GetDbContext();
+            this._serviceProvider = this.GetServiceProvider();
+
+            this._dbContext = this._serviceProvider.GetRequiredService<CheDbContext>();
+            this._mapper = this._serviceProvider.GetRequiredService<IMapper>();
         }
 
         public CheDbContext DbContext => this._dbContext;
 
-        public IMapper Mapper { get; set; }
+        public IMapper Mapper => this._mapper;
 
         public void Dispose()
         {
             this._dbContext.Database.EnsureDeleted();
         }
 
-        private CheDbContext GetDbContext()
+        private ServiceCollection SetServices()
         {
-            var options = new DbContextOptionsBuilder<CheDbContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
+            var services = new ServiceCollection();
 
-            var dbContext = new CheDbContext(options);
-            dbContext.Database.EnsureCreated();
+            // dbContext
+            services.AddDbContext<CheDbContext>(
+                opt => opt.UseInMemoryDatabase(Guid.NewGuid().ToString()));
 
-            return dbContext;
+            // autoMapper
+            services.AddAutoMapper(typeof(CooperativeProfile));
+
+            return services;
+        }
+
+        private ServiceProvider GetServiceProvider()
+        {
+            var services = this.SetServices();
+
+            return services.BuildServiceProvider();
         }
     }
 }
