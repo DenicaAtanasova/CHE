630eb233432121ab729c817abefb8d3003f742fe
Denica Atanasova <d.d.atanasova@gmail.com>
2020-03-13 11:13:06 +0200

    Added BaseTest class with InMemoryDb and db context

---
+++ Tests/CHE.Services.Data.Tests/BaseTest.cs
@@ -0,0 +1,41 @@
+﻿namespace CHE.Services.Data.Tests
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+
+    using AutoMapper;
+
+    using CHE.Data;
+    using CHE.Services.Mapping;
+
+    public class BaseTest : IDisposable
+    {
+        private readonly CheDbContext _dbContext;
+
+        public BaseTest()
+        {
+            this._dbContext = this.GetDbContext();
+        }
+
+        public CheDbContext DbContext => this._dbContext;
+
+        public IMapper Mapper { get; set; }
+
+        public void Dispose()
+        {
+            this._dbContext.Database.EnsureDeleted();
+        }
+
+        private CheDbContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<CheDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var dbContext = new CheDbContext(options);
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+    }
+}
