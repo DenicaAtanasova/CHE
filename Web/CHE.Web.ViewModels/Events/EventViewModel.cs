16337fe659d434c9bc730b09aabf01706ccbec5e
Denica Atanasova <d.d.atanasova@gmail.com>
2020-03-25 21:09:22 +0200

    Added title to EventViewModel

--- Web/CHE.Web.ViewModels/Events/EventViewModel.cs
+++ Web/CHE.Web.ViewModels/Events/EventViewModel.cs
@@ -6,6 +6,8 @@
     {
         public string Id { get; set; }
         
+        public string Title { get; set; }
+
         public string Description { get; set; }
 
         public DateTime StartDate { get; set; }
