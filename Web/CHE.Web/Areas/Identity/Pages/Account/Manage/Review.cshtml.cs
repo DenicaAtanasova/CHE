4f848db8540252d4e082f41e684a84ff69f6815a
Denica Atanasova <d.d.atanasova@gmail.com>
2020-06-22 10:36:15 +0300

    Removed ReviewsListViewComponent. Added _ReviewsAllPartial.

--- Web/CHE.Web/Areas/Identity/Pages/Account/Manage/Review.cshtml.cs
+++ Web/CHE.Web/Areas/Identity/Pages/Account/Manage/Review.cshtml.cs
@@ -4,27 +4,30 @@ namespace CHE.Web.Areas.Identity.Pages.Account.Manage
     using Microsoft.AspNetCore.Mvc.RazorPages;
 
     using CHE.Data.Models;
+    using CHE.Services.Data;
+    using CHE.Web.ViewModels.Reviews;
+
+    using System.Collections.Generic;
 
     public class ReviewModel : PageModel
     {
         private readonly UserManager<CheUser> _userManager;
+        private readonly IReviewsService _reviewsService;
 
-        public ReviewModel(UserManager<CheUser> userManager)
+        public ReviewModel(UserManager<CheUser> userManager, IReviewsService reviewsService)
         {
             this._userManager = userManager;
-            this.Teacher = new TeacherViewModel();
+            this._reviewsService = reviewsService;
+            this.Reviews = new List<ReviewAllViewModel>();
         }
 
-        public TeacherViewModel Teacher { get; set; }
-
-        public class TeacherViewModel
-        {
-            public string Id { get; set; }
-        }
+        public IEnumerable<ReviewAllViewModel> Reviews { get; set; }
 
         public void OnGet()
         {
-            this.Teacher.Id = this._userManager.GetUserId(this.User); ;
+            var teacherId = this._userManager.GetUserId(this.User);
+
+            this.Reviews = this._reviewsService.GetAllAsync<ReviewAllViewModel>(teacherId).GetAwaiter().GetResult();
         }
     }
 }
