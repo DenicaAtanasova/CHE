b780ff45b8ce7dd7c482222084eaba3fe636bf51
Denica Atanasova <d.d.atanasova@gmail.com>
2021-06-21 13:16:40 +0300

    Fixed issue CS0017.

--- Web/CHE.Web/Program.cs
+++ Web/CHE.Web/Program.cs
@@ -1,14 +1,13 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-
-using CHE.Web;
-
-Program.CreateHostBuilder(args).Build().Run();
-
 namespace CHE.Web
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
     public class Program
     {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
