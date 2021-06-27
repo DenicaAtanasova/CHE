8a6a292276084dadea0d16de4fb9983f9d9aa2ce
Denica Atanasova <d.d.atanasova@gmail.com>
2021-06-27 12:42:30 +0300

    Add missing migration.

---
+++ Data/CHE.Data/Migrations/20210626053654_AddCascadeDeleteOnTeacherSchedule.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CHE.Data.Migrations
+{
+    public partial class AddCascadeDeleteOnTeacherSchedule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Schedules_ScheduleId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ScheduleId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduleId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TeacherId",
+                table: "Schedules",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_TeacherId",
+                table: "Schedules",
+                column: "TeacherId",
+                unique: true,
+                filter: "[TeacherId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_AspNetUsers_TeacherId",
+                table: "Schedules",
+                column: "TeacherId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_AspNetUsers_TeacherId",
+                table: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_TeacherId",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "TeacherId",
+                table: "Schedules");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ScheduleId",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ScheduleId",
+                table: "AspNetUsers",
+                column: "ScheduleId",
+                unique: true,
+                filter: "[ScheduleId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Schedules_ScheduleId",
+                table: "AspNetUsers",
+                column: "ScheduleId",
+                principalTable: "Schedules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
