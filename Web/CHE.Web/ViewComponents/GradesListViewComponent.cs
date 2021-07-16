9d65e8cb46021a9d47ffc0154a22b2e8f5650530
Denica Atanasova <d.d.atanasova@gmail.com>
2021-07-16 10:18:16 +0300

    Restore grades list view component

---
+++ Web/CHE.Web/ViewComponents/GradesListViewComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace CHE.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+    using CHE.Services.Data;
+
+    public class GradesListViewComponent : ViewComponent
+    {
+        private readonly IGradesService _gradesService;
+
+        public GradesListViewComponent(IGradesService gradesService)
+        {
+            _gradesService = gradesService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string currentGrade = null)
+        {
+            var gradesList = await _gradesService.GetAllValuesAsync(currentGrade);
+
+            return this.View(gradesList);
+        }
+    }
+}
