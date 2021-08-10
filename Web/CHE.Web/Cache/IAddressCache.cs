57ea97a5a180e2cc2c6992bd2b1087df857bbef0
Denica Atanasova <d.d.atanasova@gmail.com>
2021-08-10 21:36:47 +0300

    Little fix.

---
+++ Web/CHE.Web/Cache/IAddressCache.cs
@@ -0,0 +1,15 @@
+﻿namespace CHE.Web.Cache
+{
+
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IAddressCache
+    {
+        void Set(string city, string neighbourhood);
+
+        Task<IEnumerable<string>> GetAsync(CacheType cacheType);
+
+    }
+
+}
