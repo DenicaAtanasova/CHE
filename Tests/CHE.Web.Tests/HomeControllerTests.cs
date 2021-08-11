72cd18a54456e03a48f7c2ea0d1b7f812fd522ee
Denica Atanasova <d.d.atanasova@gmail.com>
2021-08-11 16:25:32 +0300

    Add project for web testing.

---
+++ Tests/CHE.Web.Tests/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿namespace CHE.Web.Tests
+{
+    using CHE.Web.Controllers;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexShouldReturnCorrectView()
+            => MyMvc
+                .Pipeline()
+                .ShouldMap("/")
+                .To<HomeController>(c => c.Index())
+                .Which()
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void ErrorShouldReturnCorrectView()
+            => MyMvc
+                .Pipeline()
+                .ShouldMap("/Home/Error")
+                .To<HomeController>(c => c.Error())
+                .Which()
+                .ShouldReturn()
+                .View();
+    }
+}
